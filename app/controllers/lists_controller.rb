64aa91252670a9b8caaa017931d2f0937ea46552
Qassim Osseni <qassimosseni@gmail.com>
2024-07-04 16:11:22 +0200

    ex2 fini

--- app/controllers/lists_controller.rb
+++ app/controllers/lists_controller.rb
@@ -1,13 +1,40 @@
+
 class ListsController < ApplicationController
+
   def index
+    @lists = List.all
+    # index : Récupère toutes les listes et les assigne à l'instance variable @lists.
   end
 
   def show
+    @list = List.find(params[:id])
+    # show : Récupère une liste spécifique(les ids en fonction de l'ID fourni
+    # grâce aux paramètres de la requête et l'assigne à l'instance variable @list.
   end
 
   def new
+    @list = List.new
+    # new : Initialise une nouvelle instance de List et l'assigne à l'instance variable @list.
   end
 
   def create
+    @list = List.new(list_params)
+    # create : Initialise une nouvelle instance de List avec les paramètres fournis (via list_params).
+    if @list.save
+      # Si la liste est sauvegardée avec succès, redirige vers la page listant les listes avec un message de succès.
+      redirect_to list_path(@list), notice: 'Ta liste a été créé correctement.'
+    else
+      render :new
+      # Sinon, rend le formulaire de création de liste (:new) à nouveau pour afficher les erreurs.
+
+    end
+  end
+
+  private
+
+  def list_params
+    params.require(:list).permit(:name)
+    # list_params : Fonction privée qui extrait et retourne les paramètres pour créer ou mettre à jour une liste
+    # ici dans ce cas, uniquement le nom
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,23 +1,29 @@
 Rails.application.routes.draw do
-  get 'lists/index'
-  get 'lists/show'
-  get 'lists/new'
-  get 'lists/create'
+#  get 'bookmarks/create'
+#  get 'bookmarks/destroy'
+#  get 'lists/index'
+#  get 'bookmarks/new'
+#  get 'lists/create'
+#  get 'lists/show'
+#  get 'lists/new'
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
-  # Can be used by load balancers and uptime monitors to verify that the app is live.
-  get "up" => "rails/health#show", as: :rails_health_check
-
-  get 'lists', to: 'lists#index', as: 'lists'
-  # Route pour voir mes listes
-  get 'lists/:id', to: 'lists#show', as: 'list'
-  # Route pour voir les details d'une liste.
-  get 'lists/new', to: 'lists#new', as: 'new_list'
-  # Route pour montrer le formulaire qui crééra la nouvelle liste
-  post 'lists', to: 'lists#create'
+ # # Can be used by load balancers and uptime monitors to verify that the app is live.
+ # get "up" => "rails/health#show", as: :rails_health_check
+#
+ # get 'lists', to: 'lists#index', as: 'lists'
+ # # Route pour voir mes listes
+ # get 'lists/:id', to: 'lists#show', as: 'list'
+ # # Route pour voir les details d'une liste.
+ # get 'lists/new', to: 'lists#new', as: 'new_list'
+ # # Route pour montrer le formulaire qui crééra la nouvelle liste
+ # post 'lists', to: 'lists#create'
   # Route pour créer une nouvelle liste
-
-  # Defines the root path route ("/")
-  # root "posts#index"
+  resources :lists do
+    resources :bookmarks, only: [:new, :create]
+  end
+  resources :bookmarks, only: :destroy
+  # routes pour les bookmarks !
 end
+# routes pour les bookmarks !
