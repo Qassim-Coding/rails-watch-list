98572dc2b2af099d9cca84603a5591f8039687e1
Qassim Osseni <qassimosseni@gmail.com>
2024-07-04 12:34:42 +0200

    ajout controller list

--- app/models/bookmark.rb
+++ app/models/bookmark.rb
@@ -2,5 +2,5 @@ class Bookmark < ApplicationRecord
   belongs_to :movie
   belongs_to :list
   validates :comment, length: { minimum: 6 }
-  validates :movie_id, uniqueness: { scope: :list_id, message: 'ce film a dejà été ajouté à ta liste !'}
+  validates :movie_id, uniqueness: { scope: :list_id, message:'ce film a dejà été ajouté à ta liste !'}
 end
