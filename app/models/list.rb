2b583d115da0fed2122c0a3d77fb85cc845daa3e
Qassim Osseni <qassimosseni@gmail.com>
2024-07-04 12:05:40 +0200

    ajout exo 2

--- app/models/list.rb
+++ app/models/list.rb
@@ -1,2 +1,7 @@
 class List < ApplicationRecord
+  has_many :bookmarks
+  # il faut ajouter les bookmarks avant les films, car l'ordi ne peut pas les connaitre.
+  # -> Les films se font connaitre à travers les bookmarks
+  has_many :movies, through: :bookmarks, dependent: :destroy
+  validates :name, presence: true, uniqueness: true
 end
